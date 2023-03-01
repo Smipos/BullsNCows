ddccc37bc6f96036a746c0a2ab8c2ecda02d7a0c
Smipos <smiposbis@gmail.com>
2023-03-01 05:04:08 +0300

    Накидал базовый интерфейс. Генерация числа без повторений чисел в нем.

--- BullsNCowsWindowsFormsApp/MainForm.Designer.cs
+++ BullsNCowsWindowsFormsApp/MainForm.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.hiddenNumberLabel = new System.Windows.Forms.Label();
+            this.userNumberTextBox = new System.Windows.Forms.TextBox();
+            this.bullsCountLabel = new System.Windows.Forms.Label();
+            this.cowsCountLabel = new System.Windows.Forms.Label();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hiddenNumberLabel
+            // 
+            this.hiddenNumberLabel.AutoSize = true;
+            this.hiddenNumberLabel.Font = new System.Drawing.Font("Lucida Console", 15.75F);
+            this.hiddenNumberLabel.Location = new System.Drawing.Point(51, 32);
+            this.hiddenNumberLabel.Name = "hiddenNumberLabel";
+            this.hiddenNumberLabel.Size = new System.Drawing.Size(88, 21);
+            this.hiddenNumberLabel.TabIndex = 0;
+            this.hiddenNumberLabel.Text = "label1";
+            // 
+            // userNumberTextBox
+            // 
+            this.userNumberTextBox.Location = new System.Drawing.Point(54, 102);
+            this.userNumberTextBox.Name = "userNumberTextBox";
+            this.userNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.userNumberTextBox.TabIndex = 1;
+            // 
+            // bullsCountLabel
+            // 
+            this.bullsCountLabel.AutoSize = true;
+            this.bullsCountLabel.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bullsCountLabel.Location = new System.Drawing.Point(54, 156);
+            this.bullsCountLabel.Name = "bullsCountLabel";
+            this.bullsCountLabel.Size = new System.Drawing.Size(127, 21);
+            this.bullsCountLabel.TabIndex = 2;
+            this.bullsCountLabel.Text = "Быков = 0";
+            // 
+            // cowsCountLabel
+            // 
+            this.cowsCountLabel.AutoSize = true;
+            this.cowsCountLabel.Font = new System.Drawing.Font("Lucida Console", 15.75F);
+            this.cowsCountLabel.Location = new System.Drawing.Point(54, 193);
+            this.cowsCountLabel.Name = "cowsCountLabel";
+            this.cowsCountLabel.Size = new System.Drawing.Size(127, 21);
+            this.cowsCountLabel.TabIndex = 3;
+            this.cowsCountLabel.Text = "Коров = 0";
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Font = new System.Drawing.Font("Lucida Console", 15.75F);
+            this.confirmButton.Location = new System.Drawing.Point(223, 98);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(75, 35);
+            this.confirmButton.TabIndex = 4;
+            this.confirmButton.Text = "OK";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.cowsCountLabel);
+            this.Controls.Add(this.bullsCountLabel);
+            this.Controls.Add(this.userNumberTextBox);
+            this.Controls.Add(this.hiddenNumberLabel);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label hiddenNumberLabel;
+        private System.Windows.Forms.TextBox userNumberTextBox;
+        private System.Windows.Forms.Label bullsCountLabel;
+        private System.Windows.Forms.Label cowsCountLabel;
+        private System.Windows.Forms.Button confirmButton;
     }
 }
 
--- BullsNCowsWindowsFormsApp/MainForm.cs
+++ BullsNCowsWindowsFormsApp/MainForm.cs
@@ -12,9 +12,43 @@ namespace BullsNCowsWindowsFormsApp
 {
     public partial class MainForm : Form
     {
+        string hiddenNumber = "";
+        const int numberLength = 4;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            List<int> digits = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Random random = new Random();
+            for (int i = 0; i < numberLength; i++)
+            {
+                int digitIndex = random.Next(0, digits.Count);
+                hiddenNumber += digits[digitIndex].ToString();
+                digits.RemoveAt(digitIndex);
+            }
+            hiddenNumberLabel.Text = hiddenNumber;  
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            string userNumber = userNumberTextBox.Text;
+
+            int bullsCount = CalculateBullsCount(userNumber);
+            int cowsCount = CalculateCowsCount(userNumber);
+        }
+
+        private int CalculateCowsCount(string userNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        private int CalculateBullsCount(string userNumber)
+        {
+            throw new NotImplementedException();
         }
     }
 }
